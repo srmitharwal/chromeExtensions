23c7dd8aa270da15bce44723ca72bb0899b484d8
root <root@next-hyd-9518.nexteducation.com>
2018-10-19 20:22:47 +0530

    code cleanup

--- background.js
+++ background.js
@@ -11,37 +11,30 @@ chrome.browserAction.onClicked.addListener(function(tab){
 
 
 chrome.commands.onCommand.addListener(function (command){
-	
-
 	if(command=="tab-change"){
-
-		
-			chrome.windows.getCurrent({populate:true},function(currWindow){
-				var index=currWindow.id;
-				console.log(currWindow);
+		chrome.windows.getCurrent({populate:true},function(currWindow){
+			var index=currWindow.id;
+			console.log(currWindow);
 				//console.log(windows);
-				chrome.windows.getAll({populate:true}, function (windows){
-					var size=windows.length;
-					console.log(windows);
-					for(var i=0;i<size;i++){
-						if(windows[i].id==currWindow.id){
-							chrome.windows.update(currWindow.id,{focused:false},function(updatedWindow){
-								console.log(updatedWindow);
-								console.log("window changed");
-							});
+			chrome.windows.getAll({populate:true}, function (windows){
+				var size=windows.length;
+				console.log(windows);
+				for(var i=0;i<size;i++){
+					if(windows[i].id==currWindow.id){
+						chrome.windows.update(currWindow.id,{focused:false},function(updatedWindow){
+							console.log(updatedWindow);
+							console.log("window changed");
+						});
 							
-							chrome.windows.update(windows[(i+1)%size].id,{focused:true},function(updatedWindow){
-									console.log(updatedWindow);
-									console.log("window changed");
-								});
-							return;
-						}
+						chrome.windows.update(windows[(i+1)%size].id,{focused:true},function(updatedWindow){
+							console.log(updatedWindow);
+							console.log("window changed");
+						});
+						return;
 					}
-				});
-			
+				}
+			});
 		});
-	
-	
 	}
 });
 
