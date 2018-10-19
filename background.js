2d379d95e9742ea7f2c651b0082b50a0056bad48
root <root@next-hyd-9518.nexteducation.com>
2018-10-19 17:30:16 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,57 @@
+chrome.browserAction.onClicked.addListener(function(tab){
+
+	 chrome.bookmarks.create({ 'parentId':'1',
+                               'title': tab.title,
+                           		'url':tab.url},
+                              function(newFolder) {
+        console.log("added folder: " + newFolder.title);
+      });
+
+});
+
+
+chrome.commands.onCommand.addListener(function (command){
+	
+
+	if(command=="tab-change"){
+
+		
+			chrome.windows.getCurrent({populate:true},function(currWindow){
+				var index=currWindow.id;
+				console.log(currWindow);
+				//console.log(windows);
+				chrome.windows.getAll({populate:true}, function (windows){
+					var size=windows.length;
+					console.log(windows);
+					for(var i=0;i<size;i++){
+						if(windows[i].id==currWindow.id){
+							chrome.windows.update(currWindow.id,{focused:false},function(updatedWindow){
+								console.log(updatedWindow);
+								console.log("window changed");
+							});
+							
+							chrome.windows.update(windows[(i+1)%size].id,{focused:true},function(updatedWindow){
+									console.log(updatedWindow);
+									console.log("window changed");
+								});
+							return;
+						}
+					}
+				});
+			/*for(var i=0;i<size;i++){
+				if(size==1){
+					console.log("nothing to do");
+					return;
+				}*/
+				//console.log(windows[i]);
+			
+		});
+	
+		/*chrome.tabs.query({active:false,currentWindow:true}, function (tabs){
+			
+		});*/
+	}
+});
+
+  
+
